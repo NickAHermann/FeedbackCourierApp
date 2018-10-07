6a957ffa1b991c62872c2261e26bf11cdb17301b
NickAHermann <NickAHermann@gmail.com>
2018-10-07 02:19:18 -0500

    add billing and client side application

---
+++ client/src/components/Billing.js
@@ -0,0 +1,25 @@
+import React, { Component } from "react";
+import StripeCheckout from "react-stripe-checkout";
+import { connect } from "react-redux";
+import * as actions from "../actions";
+
+class Billing extends Component {
+  render() {
+    return (
+      <StripeCheckout
+        name="Feedback Courier"
+        description="$5 for 5 Mass Survey Credits"
+        amount={500}
+        token={token => this.props.handleToken(token)}
+        stripeKey={process.env.REACT_APP_STRIPE_KEY}
+      >
+        <button className="btn">Add Credits with Stripe</button>
+      </StripeCheckout>
+    );
+  }
+}
+
+export default connect(
+  null,
+  actions
+)(Billing);
---
+++ client/src/components/Landing.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+const Landing = () => {
+  return (
+    <div style={{ textAlign: "center" }}>
+      <h1>Feedback Courier</h1>
+      Collect feedback from the users of your product!
+    </div>
+  );
+};
+
+export default Landing;
